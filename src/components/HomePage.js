0c603ef1777bbb9af56a3f54c3418ca1d3ca0d66
Falcao1992 <eduardo.marselle@gmail.com>
2021-02-11 15:11:09 +0100

    change height homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -90,7 +90,7 @@ const spinning = keyframes`
 const ContainerPage = styled.div`
     position: relative;
     width: 100%;
-    height: 92vh;
+    height: 90vh;
     background-color: #14161a;
     overflow: hidden;
     
